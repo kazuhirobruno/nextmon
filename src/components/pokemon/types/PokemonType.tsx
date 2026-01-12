653fe0e1e997483073ba9e522cc9ed16dd841eb0
Bruno <brunokazuhiro@gmail.com>
2026-01-12 15:34:53 -0300

    refactor: adjusts type color

--- src/components/pokemon/types/PokemonType.tsx
+++ src/components/pokemon/types/PokemonType.tsx
@@ -9,7 +9,7 @@ export default function PokemonType({ selectedPokemon }: PokemonTypeProps) {
   return (
     <div className="flex flex-row gap-1">
       {selectedPokemon.types.map(type => (
-        <span className={`bg-type-${type.type.name} rounded-lg capitalize flex-1 py-1 px-3 text-gray-700`} key={type.type.name}>{type.type.name}</span>
+        <span className={`bg-type-${type.type.name} rounded-lg capitalize flex-1 py-1 px-3`} key={type.type.name}>{type.type.name}</span>
       ))}
     </div>
   )  
